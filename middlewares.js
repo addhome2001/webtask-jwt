89b29dd6476ac62371f2643206293c044b25af40
Seven <addhome2001@yahoo.com.tw>
2019-08-09 14:01:53 +0800

    improve mongoose error messages

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 module.exports.db = (req, res, next) => {
+  mongoose.Promise = global.Promise;
   mongoose.connect(req.webtaskContext.secrets.MONGO_URL);
+  mongoose.connection
+    .once('open', () => console.log('Connected to MongoDB instance.'))
+    .on('error', error => console.log('Error connecting to MongoDB:', error));
+
   req.on('close', () => {
     mongoose.connection.close();
   });
